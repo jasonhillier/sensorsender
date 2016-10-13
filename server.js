f621a87eff08c6ae523fd1f20abf9398700c95c3
jasonhillier <jason@paviasystems.com>
2016-10-12 23:39:37 -0700

    fix config

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var _Orator = require(__dirname+'/node_modules/headlight-node-sdk/server/Headlig
 	{
 		Product:'Headlight-SDK-App-SensorSender',
 		// The folder to serve static web files from for this app.  By default, use the Stage folder.
-		StaticContentFolder: __dirname+'/stage/',
-
-		"Sensors":
-		{
-			"audio":
-			{
-				"displayName":"Audio Threshold Sensor",
-				"type": "pseudo",
-				"range": "bool"
-			}
-		}
+		StaticContentFolder: __dirname+'/stage/'
 	}).orator(function customRouteHandler(pOrator)
 	{
 		var _RPIOWebServices = new (require('rpio-webservices'))(pOrator);
